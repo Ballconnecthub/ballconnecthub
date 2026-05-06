84a75ebc2d09f31c8290e808719577ebc9f95453
Ballconnecthub <info@ballconnecthub.com>
2026-05-06 18:37:03 +0200

    Add custom domain to allowed hosts

--- ballconnecthub/settings.py
+++ ballconnecthub/settings.py
@@ -28,7 +28,13 @@ SECRET_KEY = 'django-insecure-&6!9ky6%x9=sb55s8ap(-_jv4l9p$tsb474_orvpe3usn*z%wz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "ballconnecthub.onrender.com",
+    "ballconnecthub.com",
+    "www.ballconnecthub.com",
+]
 
 
 # Application definition
@@ -47,6 +53,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,6 +138,8 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 LOGIN_URL = "/login/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 import os
