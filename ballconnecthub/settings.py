f8229a8595b1d80b4d880dea9064cab3e62b81df
Ballconnecthub <info@ballconnecthub.com>
2026-05-09 08:22:41 +0200

    Fix password reset email settings

--- ballconnecthub/settings.py
+++ ballconnecthub/settings.py
@@ -99,7 +99,20 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get("info@ballconnecthub.com")
+EMAIL_HOST_PASSWORD = os.environ.get("Amahitamo1962%")
+
+DEFAULT_FROM_EMAIL = "info@ballconnecthub.com"
+
+LOGIN_URL = "/login/"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = "/login/"
 
